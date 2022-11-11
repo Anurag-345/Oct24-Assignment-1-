92794ab50484787eef49308dd365d25763c375e9
Anurag Nayajk <anuragnayak345@gmail.com>
2022-11-12 01:29:07 +0530

    initial commit

---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <>
+      <div className="main">
+        {/* Heading */}
+        <h1>About Us</h1>
+
+        {/* Body */}
+        <p>
+          At Zappos.com our purpose is simple to live and deliver WOW.
+          <br />
+          <br/>
+          Twenty years ago, we began as a small online retailer that only sold
+          shoes. Today, we still sell shoes as well as clothing, handbags,
+          accessories, and more. That "more" is providing the very best customer
+          service, customer experience, and company culture. We aim to inspire
+          the world by showing it's possible to simultaneously deliver happiness
+          to customers, employees, vendors, shareholders, and the community in a
+          long-term, sustainable way. We hope that in the future people won't
+          even realize we started selling shoes online. Instead, they'll know
+          Zappos as a service company that just happens to sell____.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutUs;
